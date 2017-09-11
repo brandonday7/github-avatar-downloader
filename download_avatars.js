fac2d6fdefb3286a40508b62ba5573371e881c8b
Brandon Day <brandontday7@gmail.com>
2017-09-11 18:19:12 +0000

    sends a valud request including a user agent header

--- download_avatars.js
+++ download_avatars.js
@@ -6,11 +6,28 @@ var GITHUB_TOKEN = '77c654116d70e01b4d42e819c69243f56168d60d';
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var USER_AGENT_HEADER = 'Github Avatar Downloader - Student Project';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': USER_AGENT_HEADER}
+  }
+  request.get(options)
+    .on('error', function() {
+      throw err;
+    })
+    .on('response', function(response) {
+      console.log("Response Status Code: ", response.statusCode);
+      console.log("Status message: ", response.statusMessage)
+    });
+
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+
 
