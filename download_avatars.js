eb837c7a6f2e147cd50db93405b780a8626204da
Brandon Day <brandontday7@gmail.com>
2017-09-11 20:36:48 +0000

    program will not run unless repo name and owner exist

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,43 @@
-var request = require('request');
-var fs = require('fs');
+let request = require('request');
+let fs = require('fs');
 console.log('Welcome to the Github Avatar Downloader');
+let args = process.argv;
 
-var GITHUB_USER = 'brandonday7';
-var GITHUB_TOKEN = '77c654116d70e01b4d42e819c69243f56168d60d';
+let GITHUB_USER = 'brandonday7';
+let GITHUB_TOKEN = '77c654116d70e01b4d42e819c69243f56168d60d';
+
+const REPO_NAME = args[2];
+const REPO_OWNER = args[3];
+
+if (REPO_NAME && REPO_OWNER) {
+
+  getRepoContributors(REPO_OWNER, REPO_NAME, function(err, result) {
+  for (contributor in result) {
+    let url = result[contributor]['avatar_url'];
+    console.log(result[contributor].login, ": ", url);
+    let filePath = './avatars/' + result[contributor].login + '.jpg'; //make sure there is an empty avatars folder on comp
+    downloadImageByURL(url, filePath);
+  }
+});
+
+}
+
+
+else {
+  console.log("A repo name and owner must be input!");
+  console.log("Example: ");
+  console.log("\tnode download_avatars.js jquery jquery");
+}
+
+
+
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  var USER_AGENT_HEADER = 'Github Avatar Downloader - Student Project';
-  var options = {
+  let requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  let USER_AGENT_HEADER = 'Github Avatar Downloader - Student Project';
+  let options = {
     url: requestURL,
     headers: {
       'User-Agent': USER_AGENT_HEADER}
@@ -20,11 +49,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
       throw error;
     }
 
-    var list = JSON.parse(body);
+    let list = JSON.parse(body);
 
     cb(null, list);
   })
 }
+
+
+
 
 
 function downloadImageByURL(url, filePath) {
@@ -42,13 +74,6 @@ function downloadImageByURL(url, filePath) {
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  for (contributor in result) {
-    let url = result[contributor]['avatar_url'];
-    console.log(result[contributor].login, ": ", url);
-    let filePath = './avatars/' + result[contributor].login + '.jpg'; //make sure there is an empty avatars folder on comp
-    downloadImageByURL(url, filePath);
-  }
-});
+
 
 
