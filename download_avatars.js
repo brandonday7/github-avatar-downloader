6d06c67aad651199aac9627319d786709a13c519
Brandon Day <brandontday7@gmail.com>
2017-09-11 20:05:57 +0000

    loop through links to photos

--- download_avatars.js
+++ download_avatars.js
@@ -31,8 +31,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  for (link in result) {
+    console.log(result[link]);
+  }
 });
 
 
