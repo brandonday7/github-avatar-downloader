a31df3cc92d259b2eb861182f3783a4cc99d8e1f
Brandon Day <brandontday7@gmail.com>
2017-09-11 17:47:25 +0000

    generated the URL to get to the contributors page

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,12 @@
 var request = require('request');
 console.log('Welcome to the Github Avatar Downloader');
 
+var GITHUB_USER = 'brandonday7';
+var GITHUB_TOKEN = '77c654116d70e01b4d42e819c69243f56168d60d';
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
