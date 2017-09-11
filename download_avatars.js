13f1d05f132ee8f88cf5c896f73be3e9f27185f6
Brandon Day <brandontday7@gmail.com>
2017-09-11 23:04:21 +0000

    added error handlers

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,29 @@
 let request = require('request');
 let fs = require('fs');
+let mkdirp = require('mkdirp');
 require('dotenv').config();
 
 console.log('Welcome to the Github Avatar Downloader');
 let args = process.argv;
+if (args.length !== 4)
+{
+  properInput();
+  return;
+}
+
+//validate .env file
+//ensure there is an env file
+//ensure the file contains GITHUB_USER and GITHUB_TOKEN variables
+//ensure these variables are not empty
+if (!process.env.GITHUB_USER) {
+  console.log("There is no username in your dotenv file. Please add one to your dotenv file.");
+  return;
+}
+else if (!process.env.GITHUB_TOKEN) {
+  console.log("There is no token in your dotenv file. Please add one to your dotenv file.");
+  return;
+}
+
 
 let GITHUB_USER = process.env.GITHUB_USER;
 let GITHUB_TOKEN = process.env.GITHUB_TOKEN;
@@ -14,24 +34,38 @@ const REPO_OWNER = args[3];
 if (REPO_NAME && REPO_OWNER) {
 
   getRepoContributors(REPO_OWNER, REPO_NAME, function(err, result) {
+    mkdirp('./avatars', function(err) {
+      if (err) {
+        console.log("The download directory does not exist.");
+      }
+      else {
+        for (contributor in result) {
+        let url = result[contributor]['avatar_url'];
+        let filePath = './avatars/' + result[contributor].login + '.jpg'; //make sure there is an empty avatars folder on comp
+        downloadImageByURL(url, filePath);
+        }
+      }
+    })
+
   for (contributor in result) {
     let url = result[contributor]['avatar_url'];
-    console.log(result[contributor].login, ": ", url);
     let filePath = './avatars/' + result[contributor].login + '.jpg'; //make sure there is an empty avatars folder on comp
     downloadImageByURL(url, filePath);
   }
 });
 
+
 }
 
-
 else {
-  console.log("A repo name and owner must be input!");
+  properInput();
+}
+
+function properInput() {
+  console.log("Please input a valid repo owner and name!");
   console.log("Example: ");
   console.log("\tnode download_avatars.js repoOwner repoName");
 }
-
-
 
 
 
@@ -46,14 +80,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 
   request.get(options, function(error, response, body) {
-    if (error) {
-      console.log("There was an error! Error number: ", error)
-      throw error;
+    try {
+      let list = JSON.parse(body);
+      cb(null, list);
     }
-
-    let list = JSON.parse(body);
-
-    cb(null, list);
+    catch (e) {
+      console.log("There was an error downloading the avatars. Ensure name and owner are valid. ");
+      return;
+    }
   })
 }
 
@@ -63,19 +97,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
-    .on('error', function(err) {
-      console.log("There was an error!")
-    })
     .on('response', function() {
       console.log("Downloading...");
     })
     .pipe(fs.createWriteStream(filePath))
+    .on('error', function(err) {
+      console.log("There was an error downloading the images!")
+    })
     .on('finish', function() {
       console.log("Donwload complete!")
     });
 }
-
-
 
 
 
