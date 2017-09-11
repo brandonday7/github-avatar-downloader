ad0046b8b0cc2eed56f3e0058ec8f1c6124c2124
Brandon Day <brandontday7@gmail.com>
2017-09-11 20:16:33 +0000

    wrote function to download image based on a link in parameter

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 console.log('Welcome to the Github Avatar Downloader');
 
 var GITHUB_USER = 'brandonday7';
@@ -25,16 +26,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
     cb(null, avatarURLs);
   })
-
-
 }
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  for (link in result) {
-    console.log(result[link]);
-  }
-});
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function(err) {
+      console.log("There was an error!")
+    })
+    .on('response', function() {
+      console.log("Downloading...");
+    })
+    .pipe(fs.createWriteStream(filePath))
+    .on('finish', function() {
+      console.log("Donwload complete!")
+    });
+}
 
 
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   for (link in result) {
+//     console.log(result[link]);
+//   }
+// });
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466avatars/kvirani.jpg", "./pic.jpg")
 
